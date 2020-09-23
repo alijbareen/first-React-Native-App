f78b23da5db9eeb47e7b98b47a1d3ef9009eb2a7
alijb <alijb@post.bgu.ac.il>
2020-09-24 02:58:00 +0300

    ok

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const ListScreen = () => {
+  return <Text style={styles.textStyle}>List</Text>;
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 45,
+  },
+});
+
+export default ListScreen;
