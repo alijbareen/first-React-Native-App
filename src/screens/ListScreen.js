bcdb70fa9722a9016eb3e784db6ad62e237e465f
alijb <alijb@post.bgu.ac.il>
2020-09-24 03:13:19 +0300

    ok

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, FlatList } from "react-native";
 
 const ListScreen = () => {
-  return <Text style={styles.textStyle}>List</Text>;
+  const friends = [
+    { name: "friend1" },
+    { name: "friend2" },
+    { name: "friend3" },
+    { name: "friend4" },
+    { name: "friend5" },
+    { name: "friend6" },
+    { name: "friend7" },
+    { name: "friend8" },
+    { name: "friend9" },
+    { name: "friend10" },
+  ];
+  return (
+    <FlatList
+      data={friends}
+      renderItem={({ item }) => {
+        return <Text>{item.name}</Text>;
+      }}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
