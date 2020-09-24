67cc5615636aeabcfc1211f20ab2f01766910d81
alijb <alijb@post.bgu.ac.il>
2020-09-24 03:17:06 +0300

    update Flatlist

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -16,6 +16,9 @@ const ListScreen = () => {
   ];
   return (
     <FlatList
+      keyExtractor={(friend) => {
+        friend.name;
+      }}
       data={friends}
       renderItem={({ item }) => {
         return <Text>{item.name}</Text>;
